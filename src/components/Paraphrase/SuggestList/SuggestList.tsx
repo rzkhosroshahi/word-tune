b145b93a33bc6b4d98f9777e2d94686e77af6248
Reza.Khosroshahi <reza.khosroshahi@snapp.cab>
2023-04-30 01:22:53 +0330

    chore: improve dark style in suggestion list

--- src/components/Paraphrase/SuggestList/SuggestList.tsx
+++ src/components/Paraphrase/SuggestList/SuggestList.tsx
@@ -51,8 +51,8 @@ export const SuggestList = (props: SuggestListProps) => {
 
 function getSuggestionClass (loading: boolean) {
   return clsx({
-    'flex align-middle justify-between transition-all w-full p-2 rounded bg-white text-sm font-medium cursor-pointer select-none text-zinc-800': true,
-    'bg-white dark:bg-slate-400 active:bg-violet-200 dark:active:bg-slate-300': true,
+    'flex align-middle justify-between transition-all w-full p-2 rounded bg-white text-sm font-medium cursor-pointer select-none': true,
+    'text-zinc-800 dark:text-zinc-300 bg-white dark:bg-slate-700 active:bg-violet-200 dark:active:bg-slate-300': true,
     'animate-pulse': loading
   })
 }
